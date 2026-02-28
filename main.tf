f3c557c6ca5e3b414c4babc887631465a330673d
rtenesaca <rtenesaca@opensip.tech>
2026-02-28 14:46:45 -0500

    add terraform init to workflow

--- main.tf
+++ main.tf
@@ -1,25 +1,30 @@
-module "network_vpc" {
-  source       = "app.terraform.io/opensip/network/google"
-  version      = "1.0.0"
-  network_name = var.network_name
-  subnets = {
-    "web-subnet" = { cidr = "192.168.16.0/24", region = "us-central1" }
-  }
+# module "network_vpc" {
+#   source       = "app.terraform.io/opensip/network/google"
+#   version      = "1.0.0"
+#   network_name = var.network_name
+#   subnets = {
+#     "web-subnet" = { cidr = "192.168.16.0/24", region = "us-central1" }
+#   }
 
-}
+# }
 
-module "vpc_instance" {
-  source = "app.terraform.io/opensip/moduleinstances/google"
-  #source = "./terraform-google-moduleinstances"
-  version = "1.0.5"
+# module "vpc_instance" {
+#   source = "app.terraform.io/opensip/moduleinstances/google"
+#   #source = "./terraform-google-moduleinstances"
+#   version = "1.0.5"
 
-  instance_name   = var.vm_name
-  instance_image  = var.vm_image
-  instance_labels = var.vm_labels
-  instance_subnet = "web-subnet"
-  instance_tags   = var.vm_tags
-  instance_vpc    = var.network_name
-  instance_zone   = var.vm_zone
-  instance_type   = var.vm_type
+#   instance_name   = var.vm_name
+#   instance_image  = var.vm_image
+#   instance_labels = var.vm_labels
+#   instance_subnet = "web-subnet"
+#   instance_tags   = var.vm_tags
+#   instance_vpc    = var.network_name
+#   instance_zone   = var.vm_zone
+#   instance_type   = var.vm_type
 
+# }
+
+resource "local_file" "nota_final" {
+  content  = "Certifico que rtenesaca completo el laboratorio de IaC."
+  filename = "certificacion.txt"
 }
--- providers.tf
+++ providers.tf
@@ -3,8 +3,13 @@ terraform {
     google = {
       source = "hashicorp/google"
     }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.4.0"
+    }
   }
 }
+
 provider "google" {
   project     = var.project_name
   region      = var.region
